003ba251cd0bd5bb9de771ff1e57006163855d1a
Jorge Lucas <jorgelucas77@gmail.com>
2022-10-26 17:23:31 +0100

    conteudo criado

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "photo":"https://static0.cbrimages.com/wordpress/wp-content/uploads/2021/01/Star-Wars-Skywalker-Saga-Movies.jpg",
+    "title":"Serie Starwars",
+    "description":"ovlslfslflsflsdfskflksfslf  kfsfksdfkl la"
+  },
+  {
+    "id":"2",
+    "photo":"https://th.bing.com/th/id/OIP.4u40cq4CxkiYDVxA55xpGQHaEK?pid=ImgDet&rs=1",
+    "title":"Novo episódio de Andor",
+    "description":"O inicio da revolução"
+  },
+  {
+    "id":"3",
+    "photo":"https://th.bing.com/th/id/OIP.sN7uFVfXWwU-8NDzbLdjngHaEK?pid=ImgDet&rs=1",
+    "title":"Novo episódio de Mandalorian",
+    "description":"A série mais fixe e com o Grogu"
+  },
+  {
+    "id":"4",
+    "photo":"https://th.bing.com/th/id/R.10a90da0fa108578dccce3675f6493bd?rik=99oTqZny8YLJMQ&pid=ImgRaw&r=0",
+    "title":"Novo episódio Boba Fett 2",
+    "description":"é só mesmo Boba"
+  }
+]
